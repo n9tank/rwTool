eceee2a3bcca18a8f6a3962283b8fd4e2ad8bdaa
n9tank <n9tank@163.com>
2024-12-25 22:23:56 +0800

    内联一个方法并跳过字节

--- rwmodP/app/src/main/java/rust/BMFind.java
+++ rwmodP/app/src/main/java/rust/BMFind.java
@@ -6,13 +6,30 @@ public class BMFind {
  public byte bad[];
  public byte good[];
  public byte drc[];
- public BMFind(byte drc[]) {
+ public BMFind(byte drc[], boolean re) {
+  if (re)reverse(drc);
   bad = bad(drc);
   good = good(drc);
   this.drc = drc;
  }
  public int indexOf(byte[] src, int start, int end) {
   return BMFind(src, drc, good, bad, start, end);
+ }
+ public int lastIndexOf(byte[] src, int start, int end) {
+  return lastBMFind(src, drc, good, bad, start, end);
+ }
+ //good方法实在看不懂了，翻转将就一下
+ public static void reverse(byte[] src) {
+  int len=src.length;
+  int nlen=len - 1;
+  int l=len >> 1;
+  for (int i = 0; i < l; i++) {
+   byte a=src[i];
+   int ni=nlen - i;
+   byte b=src[ni];
+   src[i] = b;
+   src[ni] = a;
+  }
  }
  public static byte[] bad(byte src[]) {
   byte[] list=new byte[128];
@@ -29,9 +46,7 @@ public class BMFind {
   int nlen=len - 1;
   for (int j=len;--j >= 0;) {
    int k=j;
-   for (int i=0;++k < len;++i) {
-    if (src[k] != src[i])break;
-   }
+   for (int i=0;++k < len && src[k] == src[i];++i);
    if (k == len)lastpos = j + 1;
    k = nlen - j;
    list[k] = (byte)(lastpos + k);
@@ -46,13 +61,26 @@ public class BMFind {
  public static int BMFind(byte src[], byte drc[], byte good[], byte bad[], int start, int end) {
   int plen=drc.length;
   int nlen=plen - 1;
-  int i,j=0;
+  int i,j;
   for (i = nlen + start;i < end;) {
    for (j = plen;src[i] == drc[--j];--i) {
     if (j == 0)return i;
    }
    int c=src[i];
    i += Math.max(good[nlen - j], c < 0 ?nlen: bad[c]);
+  }
+  return -1;
+ }
+ public static int lastBMFind(byte src[], byte drc[], byte good[], byte bad[], int start, int end) {
+  int plen=drc.length;
+  int nlen = drc.length - 1;
+  int i,j;
+  for (i = end - plen;i >= start;) {
+   for (j = plen;src[i] == drc[--j]; ++i) {
+    if (j == 0) return i;
+   }
+   int c = src[i];
+   i -= Math.max(good[nlen - j], c < 0 ? nlen : bad[c]);
   }
   return -1;
  }
